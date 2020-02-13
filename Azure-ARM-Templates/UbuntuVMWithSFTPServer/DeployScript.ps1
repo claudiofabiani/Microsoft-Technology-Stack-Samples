288c0ea9262846264450fc27e41213860c4ca4e8
Claudio Fabaini <fabbio91@hotmail.it>
2020-02-13 22:51:53 +0100

    add sftp vm

---
+++ Azure-ARM-Templates/UbuntuVMWithSFTPServer/DeployScript.ps1
@@ -0,0 +1,9 @@
+﻿Login-AzAccount
+
+$timeDeploy = Get-Date -format "yyyyMMddTHHmmssffff"
+$deploymentName ="deploy"+ $timeDeploy
+$resourceGroupName ="Nintex"
+$templateUri="https://nintexdiag748.blob.core.windows.net/armtemplate/azuredeploy.json"
+$templateParameterUri="https://nintexdiag748.blob.core.windows.net/armtemplate/azuredeploy.parameters.json"
+
+New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateParameterUri $templateParameterUri -TemplateUri $templateUri
