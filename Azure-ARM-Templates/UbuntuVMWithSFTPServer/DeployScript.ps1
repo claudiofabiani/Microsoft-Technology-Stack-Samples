4aed39a929d97102546006957762ac19035a9a21
claudiofabiani <50480652+claudiofabiani@users.noreply.github.com>
2020-02-14 18:59:37 +0100

    Update DeployScript.ps1

--- Azure-ARM-Templates/UbuntuVMWithSFTPServer/DeployScript.ps1
+++ Azure-ARM-Templates/UbuntuVMWithSFTPServer/DeployScript.ps1
@@ -3,7 +3,7 @@
 $timeDeploy = Get-Date -format "yyyyMMddTHHmmssffff"
 $deploymentName ="deploy"+ $timeDeploy
 $resourceGroupName ="Nintex"
-$templateUri="https://nintexdiag748.blob.core.windows.net/armtemplate/azuredeploy.json"
-$templateParameterUri="https://nintexdiag748.blob.core.windows.net/armtemplate/azuredeploy.parameters.json"
+$templateUri="https://<storageaccountname>.blob.core.windows.net/<containername>/azuredeploy.json"
+$templateParameterUri="https://<storageaccountname>.blob.core.windows.net/<containername>/azuredeploy.parameters.json"
 
 New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateParameterUri $templateParameterUri -TemplateUri $templateUri
