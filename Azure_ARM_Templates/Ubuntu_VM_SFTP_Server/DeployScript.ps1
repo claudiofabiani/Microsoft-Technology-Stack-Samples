96ba3069d0830e7fda28208e811217c49bbe63c0
Claudio Fabaini <fabbio91@hotmail.it>
2020-02-22 21:40:40 +0100

    add consumption and elastic plan app function

--- Azure_ARM_Templates/Ubuntu_VM_SFTP_Server/DeployScript.ps1
+++ Azure_ARM_Templates/Ubuntu_VM_SFTP_Server/DeployScript.ps1
@@ -3,7 +3,7 @@
 $timeDeploy = Get-Date -format "yyyyMMddTHHmmssffff"
 $deploymentName ="deploy"+ $timeDeploy
 $resourceGroupName ="Nintex"
-$templateUri="https://<storageaccountname>.blob.core.windows.net/<containername>/azuredeploy.json"
-$templateParameterUri="https://<storageaccountname>.blob.core.windows.net/<containername>/azuredeploy.parameters.json"
+$templateUri="https://<>.blob.core.windows.net/armtemplate/azuredeploy.json"
+$templateParameterUri="https://<>.blob.core.windows.net/armtemplate/azuredeploy.parameters.json"
 
 New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateParameterUri $templateParameterUri -TemplateUri $templateUri
